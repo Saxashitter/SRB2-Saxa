d5db1c6dfed784e232ea7d38956741028ac1bb98
Saxashitter <saxashitters@realemail.com>
2026-04-21 10:14:25 -0400

    Fix launching on Android (My bad!), Adds proper documentation for touch buttons.

--- android/app/src/main/java/org/stjr/srb2/SRB2Game.java
+++ android/app/src/main/java/org/stjr/srb2/SRB2Game.java
@@ -2,17 +2,31 @@ package org.stjr.srb2;
 
 import org.libsdl.app.SDLActivity;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class SRB2Game extends SDLActivity {
+	public static TouchControls masterTouchClass;
+	public static boolean checkPermission(String permission) {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+			return true;
+
+		Activity activity = (Activity)getContext();
+        return activity.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED;
+    }
+	public static boolean inMultiWindowMode() {
+		if (Build.VERSION.SDK_INT >= 24) {
+            return SRB2Game.mSingleton.isInMultiWindowMode();
+		}
+		return false;
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// Create your controls
-		TouchControls controls = new TouchControls(this);
-
-		// Add them to the existing SDL layout so they appear on top
-		mLayout.addView(controls);
+		TouchControls masterTouchClass = new TouchControls(this);
+		mLayout.addView(masterTouchClass);
 	}
 }
--- android/app/src/main/java/org/stjr/srb2/TouchControls.java
+++ android/app/src/main/java/org/stjr/srb2/TouchControls.java
@@ -17,9 +17,36 @@ public class TouchControls extends View {
     private static final float finalJoystickRadius = 150;
     private static final float finalJoystickSpacing = 100;
     private final TouchButton[] buttons = {
-            new TouchJoystick(0f, 1f, finalJoystickRadius + finalJoystickSpacing, -finalJoystickRadius - finalJoystickSpacing, finalJoystickRadius - finalJoystickSpacing, finalJoystickSpacing, KeyEvent.KEYCODE_DPAD_LEFT, KeyEvent.KEYCODE_DPAD_DOWN, KeyEvent.KEYCODE_DPAD_UP, KeyEvent.KEYCODE_DPAD_RIGHT),
-            new TouchButton(1f, 1f, -finalOffset, -finalOffset, finalRadius, "Jump", KeyEvent.KEYCODE_SPACE)
+            // First, we will create a joystick so the player can move.
+            // Although the joystick isn't a REAL one (All the buttons on it are keys!), it is still essential to offer good move to your player.
+            // It even acts like a D-Pad if you wanna treat it like that!
+            // Button positions are done in such a weird way to intuitively design them based on any resolution without resetting their positions based on width changes.
+
+            new TouchJoystick(
+                    0f, // The X scale across the screen. 0 is left, 1 is right, 0.5 is middle.
+                    1f, // The Y scale across the screen. 0 is up, 1 is down, 0.5 is middle.
+                    finalJoystickRadius, // The X offset for the button. This has pixel positioning, so use absolute positions!
+                    -finalJoystickRadius, // The Y offset for the button. This has pixel positioning, so use absolute positions!
+                    finalJoystickRadius - finalJoystickSpacing, // The radius of the joystick. The reason why I used radius - spacing here is cause the spacing extends the radius of it.
+                    finalJoystickSpacing, // The radius of the spacing of the buttons for the joystick.
+                    KeyEvent.KEYCODE_DPAD_LEFT, // The keycode for the left button.
+                    KeyEvent.KEYCODE_DPAD_DOWN, // The keycode for the down button.
+                    KeyEvent.KEYCODE_DPAD_UP, // The keycode for the up button.
+                    KeyEvent.KEYCODE_DPAD_RIGHT // The keycode for the right button.
+            ),
+
+            // Finally, a simple button.
+            new TouchButton(
+                    1f, // The X scale across the screen. 0 is left, 1 is right, 0.5 is middle.
+                    1f, // The Y scale across the screen. 0 is up, 1 is down, 0.5 is middle.
+                    -finalOffset, // The X offset for the button. This has pixel positioning, so use absolute positions!
+                    -finalOffset, // The Y offset for the button. This has pixel positioning, so use absolute positions!
+                    finalRadius, // The radius of the button.
+                    "Jump", // The text of the button. This displays on screen, but has no other use.
+                    KeyEvent.KEYCODE_SPACE // The keycode this button uses once pressed.
+            )
     };
+    private MotionEvent event;
 
     public TouchControls(Context context) {
         super(context);
